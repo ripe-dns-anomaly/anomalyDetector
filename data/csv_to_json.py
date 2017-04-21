21c039d60888cfed7cc8b784118fa7fc9b4e7b18
Moritz Mueller <moritz.muller@sidn.nl>
2017-04-21 15:09:12 +0200

    modified converter

--- data/csv_to_json.py
+++ data/csv_to_json.py
@@ -9,6 +9,9 @@ df_probes = df[['timestamp', 'nProbes']]
 
 df_median_rtt = df[['timestamp', 'q50RTT']]
 df_rtt_ranges = df[['timestamp', 'q25RTT', 'q75RTT']]
+df_rtt_ranges_q90RTT = df[['timestamp', 'q25RTT', 'q90RTT']]
+df_rtt_ranges_q90RTT['q25RTT'] = 0
+
 
 f = open('reachability.json', 'w')
 f.write(df_probes.to_json(orient='values'))
@@ -18,3 +21,6 @@ f.write(df_median_rtt.to_json(orient='values'))
 
 f = open('rtt_ranges.json', 'w')
 f.write(df_rtt_ranges.to_json(orient='values'))
+
+f = open('rtt_ranges_90.json', 'w')
+f.write(df_rtt_ranges_q90RTT.to_json(orient='values'))
