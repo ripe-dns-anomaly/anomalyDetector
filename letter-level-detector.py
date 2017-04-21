23e15c0d5d47a1c4b337d361507215abd00611f6
giovane <giovane.moura@sidn.nl>
2017-04-21 11:29:12 +0200

    letter level anomaly for f1 and f2 types done

--- letter-level-detector.py
+++ letter-level-detector.py
@@ -11,14 +11,34 @@ def letterDetector(infile,outFile):
 
     #read the input csv file
 
-    df=pd.read_csv(infile,header=True,sep=",")
+    df=pd.read_csv(infile)
+
+    #these values below are for the ENTIRE data sample;
     medianProbes=df["nProbes"].median()
     sdProbes=df["nProbes"].std()
+    probesThreshold = medianProbes - 3*sdProbes
 
+    #then now, we need to go line by line and see if it's an anomaly
 
+    for index, row in df.iterrows():
+        rownProbes=row["nProbes"]
+        #print(str(rownProbes) + "," + str(probesThreshold))
+        if rownProbes <= probesThreshold:
+            print("F1 Anomaly at : " + str(row["timestamp"]))
+    
+    #now, detect F2 types of failures:
+            
+    medianRTT=df["q50RTT"].median()
+    sdRTT=df["q50RTT"].std()
+    rttThreshold = medianRTT - 3*sdRTT
 
-    return 0
+    #then now, we need to go line by line and see if it's an anomaly
 
+    for index, row in df.iterrows():
+        rowRTT=row["q50RTT"]
+        #print(str(rownProbes) + "," + str(probesThreshold))
+        if rttThreshold <= rttThreshold:
+            print("F2 Anomaly at : " + str(row["timestamp"]))
 
 
 if len(sys.argv)==3:
