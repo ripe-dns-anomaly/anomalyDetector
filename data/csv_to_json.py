81058f8fe894b321e66170e14dd5f964441c9aae
Moritz Mueller <moritz.muller@sidn.nl>
2017-04-21 14:02:34 +0200

    blub

--- data/csv_to_json.py
+++ data/csv_to_json.py
@@ -6,10 +6,15 @@ df = pd.read_csv('k-root-ddos-20151130.csv')
 df['timestamp'] = df['timestamp']*1000
 
 df_probes = df[['timestamp', 'nProbes']]
+
 df_median_rtt = df[['timestamp', 'q50RTT']]
+df_rtt_ranges = df[['timestamp', 'q25RTT', 'q75RTT']]
 
 f = open('reachability.json', 'w')
 f.write(df_probes.to_json(orient='values'))
 
 f = open('rtt.json', 'w')
 f.write(df_median_rtt.to_json(orient='values'))
+
+f = open('rtt_ranges.json', 'w')
+f.write(df_rtt_ranges.to_json(orient='values'))
